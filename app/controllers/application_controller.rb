27491c6baa2921e00979d7c5698f70f43d0fb73b
Cory Tveten <corytveten@gmail.com>
2018-10-20 20:24:45 +0000

    add models and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secred, 'bg_secret'
+  end
+
   get '/' do
-    'Hello World'
+    erb :index
   end
 
 end
