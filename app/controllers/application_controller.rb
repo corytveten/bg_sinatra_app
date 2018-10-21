b12ce93d1b155923899020ab982f98e74a358a69
Cory Tveten <corytveten@gmail.com>
2018-10-21 15:26:51 +0000

    update app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,14 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
 
+    def current_user
+      User.find_by_id(session[:user_id]) if session[:user_id]
+
+    end
   end
 
 end
