707627d03815b5fe79b0cfeb95d171c6c59cc3ce
Cory Tveten <corytveten@gmail.com>
2018-10-29 15:11:44 +0000

    add edit function

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -39,4 +39,27 @@ class GamesController < ApplicationController
     end
   end
 
+  get '/games/:id/edit' do
+   @game = Game.find_by_id(params[:id])
+    if logged_in?
+        erb :'/games/edit_game'
+    elsif logged_in?
+        redirect '/games'
+    else
+        redirect '/login'
+    end
+  end
+
+  patch '/games/:id' do
+    @game = Game.find_by(params[:id])
+    @user = User.find_by(id: session[:user_id])
+    if logged_in? && @user.games.include?(@game) && !params["game"]["name"].empty?
+      @game.update(name: params["game"]["name"])
+      @game.save
+      redirect to "/games/#{@game.id}"
+    else
+      redirect to "/games/#{@game.id}/edit"
+    end
+  end
+
 end
